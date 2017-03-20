a4420057babf4f1308bd2c0ad010936d267de790
RafM <rkjmosiolek@gmail.com>
2017-03-20 11:49:18 +0100

    checkGuess() function added

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ var guessField = document.querySelector(".guessField");
 
 var guessCount = 1;
 var resetButton;
+
+function checkGuess(){
+    var userGuess = Number(guessField.value);
+
+    if (guessCount === 1) {
+        guesses.textContent = "Previous guesses: ";
+    }
+    guesses.textContent += userGuess + " ";
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Congratulations! You got it right!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = "GAME OVER!";
+        setGameOver();
+    } else {
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = "Last guess was too low! Try again!";
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = "Last guess was too high! Try again!";
+        }
+    }
+
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
+}
